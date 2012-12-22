55e4709bff2db485a43dfeea2bab9f399321a568
はく <haku@vaguehope.com>
2012-12-22 17:53:34 +0000

    Reduce method complexity.  Make Sonar happier.

--- src/main/java/com/vaguehope/cmstoad/Main.java
+++ src/main/java/com/vaguehope/cmstoad/Main.java
@@ -1,8 +1,11 @@
 package com.vaguehope.cmstoad;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.operator.OperatorException;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
@@ -16,32 +19,13 @@ public final class Main {
 		ProviderHelper.initProvider();
 		final PrintStream out = System.out;
 		final PrintStream err = System.err;
-		if (out == null || err == null) throw new AssertionError();
+		if (out == null || err == null) throw new AssertionError(); // NOSONAR Check this to prevent other parts of Sonar complaining about null pointer dereference.
 		final File dir = new File(".");
 		final Args args = new Args();
 		final CmdLineParser parser = new CmdLineParser(args);
 		try {
 			parser.parseArgument(rawArgs);
-			switch (args.getAction()) {
-				case KEYGEN:
-					new KeyGen(args, dir).run(out, err);
-					break;
-				case ENCRYPT:
-					new Encrypt(args, dir).run(out, err);
-					break;
-				case DECRYPT:
-					new Decrypt(args, dir).run(out, err);
-					break;
-				case INFO:
-					new Info(args).run(out, err);
-					break;
-				case BENCHMARK:
-					new Benchmark().run(out, err);
-					break;
-				case HELP:
-				default:
-					fullHelp(parser, err);
-			}
+			run(args, parser, dir, out, err);
 		}
 		catch (CmdLineException e) {
 			err.println(e.getMessage());
@@ -51,6 +35,29 @@ public final class Main {
 		catch (Exception e) {
 			err.println("An unhandled error occured.");
 			e.printStackTrace(err);
+		}
+	}
+
+	private static void run (Args args, CmdLineParser parser, File dir, PrintStream out, PrintStream err) throws CmdLineException, IOException, OperatorException, CMSException {
+		switch (args.getAction()) {
+			case KEYGEN:
+				new KeyGen(args, dir).run(out, err);
+				break;
+			case ENCRYPT:
+				new Encrypt(args, dir).run(out, err);
+				break;
+			case DECRYPT:
+				new Decrypt(args, dir).run(out, err);
+				break;
+			case INFO:
+				new Info(args).run(out, err);
+				break;
+			case BENCHMARK:
+				new Benchmark().run(out, err);
+				break;
+			case HELP:
+			default:
+				fullHelp(parser, err);
 		}
 	}
 
